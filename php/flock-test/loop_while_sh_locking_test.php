f44eb4ff78866714f99e5ec8a01760849668c725
Marcin Cylke <marcin.cylke+github@gmail.com>
2011-02-20 21:47:24 +0100

    add example for LOCK_SH

---
+++ php/flock-test/loop_while_sh_locking_test.php
@@ -0,0 +1,23 @@
+<?php
+
+define('MAX_REPEAT_LOCK_FILE', 10);
+
+$fp = fopen("test_file", "r+");
+
+$lock_counter = 0;
+while ($lock_counter < MAX_REPEAT_LOCK_FILE) {
+    if (flock($fp, LOCK_SH | LOCK_NB)) { 
+        echo "locked file, breaking from loop";
+        break;
+    } else {
+        echo "Couldn't get the lock! Attempted $lock_counter lockings...\n";
+        usleep(500000);
+        $lock_counter+= 1;
+    }
+}
+flock($fp, LOCK_UN); 
+
+fclose($fp);
+
+?>
+
